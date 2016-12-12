3385f7424de390d1b4885bd761a5b302d2c6e17a
nieyinyin <nieyinyin@gmail.com>
2016-12-12 17:25:00 +0800

    add Realm dependencies

--- app/src/main/java/cn/onlinecache/minimalist/MyApplication.java
+++ app/src/main/java/cn/onlinecache/minimalist/MyApplication.java
@@ -2,6 +2,8 @@ package cn.onlinecache.minimalist;
 
 import android.app.Application;
 
+import io.realm.Realm;
+
 /**
  * Created by nieyinyin on 12/12/2016.
  */
@@ -11,5 +13,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Realm.init(this);
     }
 }
