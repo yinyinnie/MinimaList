164fc89473ccfd2abf6eba12a65272142453b4d5
nieyinyin <nieyinyin@gmail.com>
2016-12-12 17:20:27 +0800

    create Android Project

---
+++ app/src/main/java/cn/onlinecache/minimalist/MyApplication.java
@@ -0,0 +1,15 @@
+package cn.onlinecache.minimalist;
+
+import android.app.Application;
+
+/**
+ * Created by nieyinyin on 12/12/2016.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/cn/onlinecache/minimalist/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.onlinecache.minimalist.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.onlinecache.minimalist.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
